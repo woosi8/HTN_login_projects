0108688946ce373dc5be411d08a1f8ad09754880
woosi8 <tmfvmehek@gmail.com>
2025-05-21 10:50:45 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export",
+  // output: "export",
   ...(process.env.NODE_ENV === "production"
     ? {
         basePath: "/HTN_login_projects",
