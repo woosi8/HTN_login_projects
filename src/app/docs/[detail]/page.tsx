3b38187ecea7a0349f022e30e4a1e2646b88acc2
woosi8 <tmfvmehek@gmail.com>
2025-06-02 16:14:09 +0900

    fix - detail

--- src/app/docs/[detail]/page.tsx
+++ src/app/docs/[detail]/page.tsx
@@ -1,10 +1,14 @@
-export default function BlogPost({ params }: { params: { detail: string } }) {
-  console.log(params);
+export default async function BlogPost({
+  params,
+}: {
+  params: Promise<{ detail: string }>;
+}) {
+  const { detail } = await params; // ✅ await 사용
 
   return (
     <div>
-      <h1>블로그 포스트: {params.detail}</h1>
-      <p>이것은 {params.detail} 포스트입니다</p>
+      <h1>블로그 포스트: {detail}</h1>
+      <p>이것은 {detail} 포스트입니다</p>
     </div>
   );
 }
